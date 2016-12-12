44f75dce8aedee2ffbd0b81048c8ef13f3bcc42c
LaMariposa <megan.a.supple@gmail.com>
2016-12-12 16:14:10 +1100

    initial

--- occur2poly.r
+++ occur2poly.r
@@ -1,14 +1,19 @@
 library(alphahull)
 library(rgeos)
+library(sp)
+library(maptools)
+library(raster)
 source("/home/megan/programs/others_scripts/ahull_to_SPLDF.r")
 
 #set output director
 outdir="/home/megan/Desktop/emel_lb1234/4.analysis/gdm/"
-outfile="emel_ala_occur_sub"
+outfile="emel_alasuboccur_20k"
 setwd(outdir)
+OzCoast=shapefile("/home/megan/megan/research/gis_data/oz_coast/aus5m_coast_no_norfolk.shp")
 
 #input occurrence records
 occurcsv="/home/megan/megan/research/eucalyptus/Emelliodora/ala_occurrence/records-2016-11-23_emel_sub/records-2016-11-23.csv"
+#occurcsv="/home/megan/megan/research/eucalyptus/Emelliodora/lb_trees/1.samples_qc/emel_lb_samples.csv"
 
 #read in occurrence data
 occur=read.delim(occurcsv, sep=",", header=T)
@@ -26,18 +31,21 @@ alphahull=ahull(occur_uniq, alpha=al) #alpha hull
 alphahull_sp=ahull_to_SPLDF(alphahull) #convert to SpatialLinesDataFrame
 
 #visualize
-plot(alphashape,col=c("blue","black"),new=F)
-plot(alphahull, do.shape=T,col=c(6,4,rep(1,4)),new=F)
+plot(alphashape,col=c("blue","black"))
+plot(alphahull, do.shape=T,col=c(6,4,rep(1,4)))
+
 plot(alphahull_sp)
-points(occur$decimalLongitude, occur$decimalLatitude, new=F)
+points(occur$decimalLongitude, occur$decimalLatitude, pch=20)
+plot(OzCoast, add=T)
 
 #add buffer
-alphahull_sp_buf=gBuffer(alphahull_sp, width=1)
+alphahull_sp_buf=gBuffer(alphahull_sp, width=.18)
 
 #final polygon
 poly=SpatialPolygons(list(Polygons(list(alphahull_sp_buf@polygons[[1]]@Polygons[[1]]),ID=1)))
 plot(poly)
-points(occur$decimalLongitude, occur$decimalLatitude, new=F)
+points(occur$decimalLongitude, occur$decimalLatitude, pch=20)
+plot(OzCoast, add=T)
 
 df=data.frame(id=getSpPPolygonsIDSlots(poly))
 row.names(df)=getSpPPolygonsIDSlots(poly)
